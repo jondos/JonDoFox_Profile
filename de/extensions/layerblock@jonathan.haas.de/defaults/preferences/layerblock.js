a202412163ec12909e7742ad68a87e045d7507e6
simon <simon@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2008-07-14 09:30:56 +0000

    not needed

--- de/extensions/layerblock@jonathan.haas.de/defaults/preferences/layerblock.js
+++
@@ -1,4 +0,0 @@
-pref('layerblock.state', 0);
-pref('layerblock.showStatusText', true);
-pref('layerblock.whitelist',"news/login/logout/profil/icq/suche/search/nav/impressum/help/hilfe/links/spalte/column/content/<h1/<h2/<p/<li");
-pref('layerblock.blacklist',"layer/close/schlie/hide/popup/show/script/sponsor/shop/iframe/advert/4cash/view");
