4015401602f8219b5057cabf96b7e501951edbba
rolf <rolf@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2009-07-03 15:18:46 +0000

    en translation

--- Firefox/App/firefox/defaults/pref/firefox-l10n.js
+++
@@ -1,5 +0,0 @@
-//@line 36 "e:\builds\moz2_slave\win32_repack\build\releases\l10n-mozilla-1.9.1\de\browser\firefox-l10n.js"
-
-//@line 38 "e:\builds\moz2_slave\win32_repack\build\releases\l10n-mozilla-1.9.1\de\browser\firefox-l10n.js"
-
-pref("general.useragent.locale", "de");
