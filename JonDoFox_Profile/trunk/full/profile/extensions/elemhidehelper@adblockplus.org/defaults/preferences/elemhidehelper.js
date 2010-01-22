db51103352c67d7db0c036a9c4bbcabc9be80e81
rolf <rolf@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2010-01-22 14:35:39 +0000

    deleted unneccesary profile data

--- JonDoFox_Profile/trunk/full/profile/extensions/elemhidehelper@adblockplus.org/defaults/preferences/elemhidehelper.js
+++
@@ -1,4 +0,0 @@
-pref("extensions.adblockplus.ehh-selectelement_key", "Accel Shift K");
-pref("extensions.adblockplus.ehh.showhelp", true);
-pref("extensions.adblockplus.ehh.norequirementswarning", false);
-
