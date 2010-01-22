a946d100b78085d44755467296ba212640a17778
rolf <rolf@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2010-01-22 14:35:39 +0000

    deleted unneccesary profile data

--- full/profile/extensions/{268ad77e-cff8-42d7-b479-da60a7b93305}/install.js
+++
@@ -1,8 +0,0 @@
-const myProductName = "Groowe Search Toolbar";const myChromeName = "groowebar";const myOrgHandle = "groowebar";const myProductRegName = "groowebar";const myProductRegVersion = "1.6.5";const myJarFileName = "groowebar.jar";const mySuccessMessage = "The " + myProductName + " has been successfully installed.\n\nRestart your browser for changes to take effect.";const myErrorMessage = "Install failed!  You probably don't have appropriate permissions (write access to the mozilla/chrome directory)";
-// Installation Script
-var err = initInstall(myProductName, myProductRegName, myProductRegVersion);logComment("initInstall: " + err);
-// fChrome = getFolder("Profile","chrome");
-fChrome = getFolder("Chrome");setPackageFolder(fChrome);err = addFile("chrome/" + myJarFileName)
-logComment("addFile() returned: " + err);
-regErr = registerChrome(PACKAGE | DELAYED_CHROME, getFolder(fChrome, myJarFileName), "content/");regErr = registerChrome(SKIN | DELAYED_CHROME, getFolder(fChrome, myJarFileName), "skin/");regErr = registerChrome(LOCALE | DELAYED_CHROME, getFolder(fChrome, myJarFileName), "locale/en-US/");logComment("regChrome returned: " + regErr);var err = getLastError();
-if (err == SUCCESS){  err = performInstall();  if (err == SUCCESS)  {    alert(mySuccessMessage);  }  else  {    // alert(myErrorMessage);    cancelInstall(err)  }}else  cancelInstall(err);
