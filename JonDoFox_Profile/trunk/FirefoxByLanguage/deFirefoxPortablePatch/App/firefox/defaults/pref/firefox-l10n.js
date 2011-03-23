d2aefe742394f3b827ff6006a26f4c8e4fe63643
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2011-03-23 16:05:15 +0000

    changes in the language dependant folders; update of help file

--- JonDoFox_Profile/trunk/FirefoxByLanguage/deFirefoxPortablePatch/App/firefox/defaults/pref/firefox-l10n.js
+++
@@ -1,5 +0,0 @@
-//@line 36 "e:\builds\moz2_slave\release-mozilla-1.9.2-win32_repack_1\build\l10n-mozilla-1.9.2\de\browser\firefox-l10n.js"
-
-//@line 38 "e:\builds\moz2_slave\release-mozilla-1.9.2-win32_repack_1\build\l10n-mozilla-1.9.2\de\browser\firefox-l10n.js"
-
-pref("general.useragent.locale", "de");
