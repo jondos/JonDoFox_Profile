a5167115ff2cf4a5e85f8b5a47f98ae38871067c
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2011-07-25 08:03:38 +0000

    added pref to disable savestats feature of AdBlock Plus

--- JonDoFox_Profile/trunk/full/profile/prefs_portable_de.js
+++ JonDoFox_Profile/trunk/full/profile/prefs_portable_de.js
@@ -74,6 +74,7 @@ user_pref("extensions.adblockplus.currentVersion", "1.3.8");
 user_pref("extensions.adblockplus.showinstatusbar", true);
 user_pref("extensions.adblockplus.showintoolbar", true);
 user_pref("extensions.adblockplus.showsubscriptions", false);
+user_pref("extensions.adblockplus.savestats", false);
 user_pref("extensions.jondofox.last_version", "");
 user_pref("extensions.jondofox.new_profile", false);
 user_pref("extensions.jondofox.profile_version", "2.5.2");
