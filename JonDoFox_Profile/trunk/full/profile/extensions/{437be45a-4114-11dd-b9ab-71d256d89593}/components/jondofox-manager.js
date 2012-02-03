bde4a0aaed8db74d98f258a56c056bea6ecb5ef9
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-02-03 20:43:47 +0000

    added adblock filterlist; updated JDF

--- JonDoFox_Profile/trunk/full/profile/extensions/{437be45a-4114-11dd-b9ab-71d256d89593}/components/jondofox-manager.js
+++ JonDoFox_Profile/trunk/full/profile/extensions/{437be45a-4114-11dd-b9ab-71d256d89593}/components/jondofox-manager.js
@@ -1130,6 +1130,8 @@ JDFManager.prototype = {
             'extensions.jondofox.profile_version') !== "2.6.1" && 
           this.prefsHandler.getStringPref(
             'extensions.jondofox.profile_version') !== "2.6.2" &&
+          this.prefsHandler.getStringPref(
+            'extensions.jondofox.profile_version') !== "2.6.3" && 
           this.prefsHandler.getBoolPref('extensions.jondofox.update_warning')) {
           this.jdfUtils.showAlertCheck(this.jdfUtils.
             getString('jondofox.dialog.attention'), this.jdfUtils.
