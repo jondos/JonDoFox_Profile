60754e7d29b3741379f61927b96acf274083ec55
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-03-08 15:25:42 +0000

    updated NSIS script; minor bugfix in english rpefs file; added to missing folder in bookmark menu again

--- JonDoFox_Profile/trunk/full/profile/prefs_portable_en.js
+++ JonDoFox_Profile/trunk/full/profile/prefs_portable_en.js
@@ -51,7 +51,7 @@ user_pref("browser.search.useDBForOrder", true);
 user_pref("browser.sessionstore.enabled", false);
 user_pref("browser.sessionstore.resume_from_crash", false);
 user_pref("browser.shell.checkDefaultBrowser", false);
-user_pref("browser.startup.homepage", "http://anonymous-proxy-servers.net/en/");
+user_pref("browser.startup.homepage", "http://anonymous-proxy-servers.net/");
 user_pref("browser.startup.homepage_override.mstone", "rv:10.0");
 user_pref("browser.startup.page", 0);
 user_pref("browser.throbber.url", "http://www.mozilla.org/");
